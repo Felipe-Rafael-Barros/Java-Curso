e89601695eaa47c06bbc3be448160f79818f15f1
Felipe-Rafael-Barros <gitfelipebarros@gmail.com>
2025-03-23 19:29:02 -0300

    segunda parte sobre tipos e variaveis

--- 1_Sintaxe_Basica_Java/Sintaxe_Basica/src/MinhaClasse.java
+++ 1_Sintaxe_Basica_Java/Sintaxe_Basica/src/MinhaClasse.java
@@ -1,3 +1,5 @@
 public class MinhaClasse {
-    
+    /*  variaveis: long,byte,short,in, long,float,double
+     *  Constantes : final double VALOR_DE_PI = 3.14;  (Nome em caixa alta e final na frente vira cte)
+    */
 }
