1a28f5aaf568fc90e3ee2600ef34d412f2555b04
Felipe-Rafael-Barros <gitfelipebarros@gmail.com>
2025-03-25 23:09:13 -0300

    Parte 1 - Operadores com Java

---
+++ 1_Sintaxe_Basica_Java/Sintaxe_Basica/src/Operadores.java
@@ -0,0 +1,33 @@
+
+public class Operadores {
+    public static void main(String [] args) {
+        String concatenacao ="?";
+        concatenacao = 1+1+1+1+"1";
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+
+        int numero = 5;
+        System.out.println(numero);
+
+        int numerox = -numero;
+        System.out.println(numerox);
+
+        numerox = ++numero;
+        System.out.println(numerox);
+
+        numerox = --numero;
+        System.out.println(numerox);
+
+
+
+
+    }
+}
+/*  Crtl,Shift,p -> Create new projec
+ * 
+*/
